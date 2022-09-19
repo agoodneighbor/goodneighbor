95bfe5da861ffc6285b9166eff595b429e43f283
hi2102 <hi2102@naver.com>
2022-09-19 18:12:33 +0900

    create: install sokect.io package

---
+++ Controller/mainController.js
@@ -0,0 +1,5 @@
+// const { User } = require("../chatModel");
+
+exports.main = (req, res) => {
+	res.render("index");
+};
---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const port = 8000;
+
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const router = require("./routes");
+
+app.use("/static", express.static("static"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", router);
+
+app.set("view engine", "ejs");
+
+io.on("connection", (socket) => {});
+
+http.listen(port, () => {
+	console.log("Server port : ", port);
+});
