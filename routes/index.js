5930661a011da4c22b24b305bb9f573c0a790cfb
brilliant2102 <brilliant2102hi@gmail.com>
2022-09-22 17:08:51 +0900

    Docs: add index

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../Controller/mainController");
-router.get("/", controller.main);
+const controller = require("../Controller/MemberController");
+
+router.get("/", controller.member);
+router.get("/memberinfo", controller.get_visitors);
 
 module.exports = router;
