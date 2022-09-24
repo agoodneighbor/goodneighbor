fdd2a620ee8bad94b3ff13dce964e5daf1af1fad
brilliant2102 <brilliant2102hi@gmail.com>
2022-09-24 16:48:59 +0900

    burning sater day

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const memberController = require("../Controller/MemberController");
+const productController = require("../Controller/ProductController");
 
 router.get("/", memberController.member);
 router.post("/api/assign", memberController.asign);
-// router.post("/api/loadadress", controller.roadaddress);
+router.post("/api/login", memberController.login);
+
+// #1, 라우트 추가
+// router.post("/api/upload", productController.product);
+// router.post("/api/mywishlist", productController.wishlist);
+// router.post("/api/myorderlist", productController.orderlist);
 
 module.exports = router;
