4319d51512926714140f23ff540af8f3920eef03
hi2102 <hi2102@naver.com>
2022-09-30 15:30:59 +0900

    Docs: fix file

--- Controller/MemberController.js
+++ Controller/MemberController.js
@@ -1,6 +1,7 @@
 "use strict";
 const { request } = require("express");
 const { Member, Address } = require("../Model");
+
 exports.member = (req, res) => {
 	let is_login = false;
 	if (req.session.user !== undefined) {
@@ -11,15 +12,14 @@ exports.member = (req, res) => {
 };
 exports.asign = async (req, res) => {
 	let isOkay = true;
-	await Member.findAll({
+	const result = await Member.findAll({
 		attributes: ["user_id"],
-	}).then((result) => {
-		for (let i = 0; i < result.length; i++) {
-			if (result[i].dataValues["user_id"] === req.body.id) {
-				isOkay = false;
-			}
-		}
 	});
+
+	for (let i = 0; i < result.length; i++) {
+		if (result[i].dataValues["user_id"] === req.body.id) isOkay = false;
+	}
+
 	if (isOkay) {
 		const data = {
 			user_id: req.body.id,
@@ -31,53 +31,42 @@ exports.asign = async (req, res) => {
 			dong: req.body["구"],
 			remaining_address: req.body["동"],
 		};
-		await Address.create(address).then((result) => {
-			data["address_id"] = result.dataValues["address_id"];
-			Member.create(data).then((result) => {});
-		});
+		const secondResult = await Address.create(address);
+		data["address_id"] = secondResult.dataValues["address_id"];
+		Member.create(data);
 	}
 	res.send(isOkay);
 };
+
+// 로그인
 exports.login = async (req, res) => {
 	let isOkay = false;
-	await Member.findAll({
+	const result = await Member.findAll({
 		attributes: ["user_id", "user_pw", "member_id"],
-	}).then((result) => {
-		let currnet_member_id = undefined;
-		for (let i = 0; i < result.length; i++) {
-			if (
-				result[i].dataValues["user_id"] === req.body.id &&
-				result[i].dataValues["user_pw"] === req.body.pw
-			) {
-				isOkay = true;
-				currnet_member_id = result[i].dataValues["member_id"];
-			}
-		}
-		if (isOkay) {
-			req.session.user = currnet_member_id;
-			console.log(req.session.user);
-		}
 	});
+	let currnet_member_id = undefined;
+	for (let i = 0; i < result.length; i++) {
+		if (
+			result[i].dataValues["user_id"] === req.body.id &&
+			result[i].dataValues["user_pw"] === req.body.pw
+		) {
+			isOkay = true;
+			currnet_member_id = result[i].dataValues["member_id"];
+		}
+	}
+	if (isOkay) {
+		req.session.user = currnet_member_id;
+		console.log(req.session.user);
+	}
 	res.send(isOkay);
 };
-// exports.delete_comment = (req,res) => {
-//     Visitor.destroy({
-//         where : { id: req.body.id }
-//     }).then((result)=>{
-//         console.log( result );
-//         res.send( "삭제 성공" );
-//     })
-//     //delete from visitor where id = req.body.id
-// }
 //로그아웃
 exports.Logout = async (req, res) => {
 	if (req.session.user) {
 		//세션정보가 존재하는 경우
 		await req.session.destroy(function (err) {
 			if (err) console.log(err);
-			else {
-				res.send(true);
-			}
+			else res.send(true);
 		});
 	}
 };
@@ -88,12 +77,10 @@ exports.accessProfile = async (req, res) => {
 		is_login = true;
 	}
 	let member_id = Number(req.session.user);
-	await Member.findOne({
+	const result = await Member.findOne({
 		where: { member_id: member_id },
-	}).then((result) => {
-		console.log(result);
-		res.render("Profile", { is_login: is_login, user: result.dataValues });
 	});
+	res.render("Profile", { is_login: is_login, user: result.dataValues });
 };
 exports.FixedProfile = async (req, res) => {
 	let member_id = Number(req.session.user);
